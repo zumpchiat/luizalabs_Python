ce62a326a0960bf4e7abd53f04e775ff54667a66
zumpchiat <zumpchiat@gmail.com>
2025-10-31 18:34:41 -0300

    feat: add arquivos

---
+++ 05-Arquivos/script_leitura.py
@@ -0,0 +1,10 @@
+arquivo = open("/home/foca/Documentos/Projetos/luizalabs_Python/05-Arquivos/log.txt", "w")
+
+arquivo.write("\nNova Linha salva")
+
+
+
+
+#while len(linha := arquivo.readline()):
+#    print(linha)
+arquivo.close()
